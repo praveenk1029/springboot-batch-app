e35382d096174409a9f84cc156bbd5961c8bdc49
Praveen <praveenk1029@gmail.com>
2020-01-20 17:15:14 -0500

    Added fixedRate and fixedDelay attributes in Scheduler for scheduling the Jobs.

--- src/main/java/com/practice/springbootbatchapp/config/SchedulerConfig.java
+++ src/main/java/com/practice/springbootbatchapp/config/SchedulerConfig.java
@@ -7,12 +7,14 @@ import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteExcep
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableScheduling
+@EnableAsync
 public class SchedulerConfig {
 
     @Autowired
@@ -21,15 +23,19 @@ public class SchedulerConfig {
     private Job job;
 
     //@Scheduled(cron = "* */5 * * * *", fixedDelay = 60000)
-    @Scheduled(fixedDelay = (3*60000))
+    //@Scheduled(fixedDelay = (3*60000))//Runs every 3 minutes
+    @Async
+    @Scheduled(fixedRate = 1000)
     public String scheduleProductJob(){
         JobParameters jobParameters =
                 new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
         JobExecution jobExecution = null;
         try {
             jobExecution = jobLauncher.run(job, jobParameters);
-        } catch (JobExecutionAlreadyRunningException | JobRestartException |JobInstanceAlreadyCompleteException
-                | JobParametersInvalidException e) {
+            Thread.sleep(2*1000);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException
+                | JobInstanceAlreadyCompleteException | JobParametersInvalidException
+                | InterruptedException e) {
             e.printStackTrace();
         }
         //System.out.println("Product Job Status::::"+jobExecution.getStatus());
